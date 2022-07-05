4df354d120f4f3a4bb6334229ab907ef4e6e48e9
fsoft <fabio.rotondo@gmail.com>
2022-07-05 07:57:54 +0200

    ADD: GPLv2 License

--- downloader.py
+++ downloader.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+"""
+CPanel AWStats Downloader - by Fabio Rotondo (fabio.rotondo@gmail.com)
+Copyright (C) 2022 - Fabio Rotondo
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+
 import argparse
 import datetime
 import json
@@ -148,7 +167,7 @@ if __name__ == "__main__":
 	parser.add_argument( '-d', '--domain', help = 'cpanel domain' )
 
 	parser.add_argument ( '-m', '--max-months', type=int, default=12 )
-	parser.add_argument( '-c', '--cfg', type=str, default='cfg.json' )
+	parser.add_argument( '-c', '--config', type=str, default='cfg.json' )
 	parser.add_argument( '--no-email', action='store_true' )
 	args = parser.parse_args()
 
